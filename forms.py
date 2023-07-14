68ab354dfda6b48e14ae8e3368ad656d5438fd8d
Seth Owirodu <sowirodu@trinity.edu>
2023-07-14 18:51:55 +0000

    Fixed Website and Forms

--- forms.py
+++ forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    location = StringField('Location', validators=[DataRequired()])
+    college = StringField('College', validators=[DataRequired()])
+    major = StringField('Major', validators=[DataRequired()])
+    career = StringField('Career', validators=[DataRequired()])
+    dream_company = StringField('Dream Company', validators=[DataRequired()])
+    linkedin = StringField('LinkedIn', validators=[DataRequired()])
+    projects = StringField('Projects', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
--- mainproject.py
+++ mainproject.py
@@ -9,20 +9,17 @@ app.config['SECRET_KEY'] = 'e30b92ab25051f1ed6da06292f122baf'
 def hello_world():
     return render_template('Seo_Tech_Conference.html')
     
-@app.route('/Seo_Tech_Conference_SignUpPage.html')
-def sign_up():
-    return render_template('Seo_Tech_Conference_SignUpPage.html')
 
 @app.route('/Seo_Tech_Conference_AboutUsPage.html')
 def about_us():
     return render_template('Seo_Tech_Conference_AboutUsPage.html')
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/Seo_Tech_Conference_SignUpPage.html", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+        return redirect(url_for('hello_world')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
